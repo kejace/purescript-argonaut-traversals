0fedb8464ec38e415d947b4a4b53d34a52e5f3a3
kejace <kejace@gmail.com>
2018-06-07 12:09:37 -0600

    test compiles

--- src/Data/Argonaut/JCursor/Gen.purs
+++ src/Data/Argonaut/JCursor/Gen.purs
@@ -7,9 +7,11 @@ import Control.Monad.Gen (class MonadGen)
 import Control.Monad.Gen as Gen
 import Control.Monad.Rec.Class (class MonadRec)
 
+import Data.Maybe (fromJust)
 import Data.Argonaut.JCursor (JCursor(..))
 import Data.Char as C
 import Data.String.CodeUnits (fromCharArray)
+import Partial.Unsafe (unsafePartial)
 
 genJCursor :: forall m. MonadGen m => MonadRec m => Lazy (m JCursor) => m JCursor
 genJCursor = Gen.resize (min 10) $ Gen.sized genJCursor'
@@ -20,4 +22,4 @@ genJCursor = Gen.resize (min 10) $ Gen.sized genJCursor'
   genField = JField <$> genString <*> defer \_ -> genJCursor
   genIndex = JIndex <$> Gen.chooseInt 0 1000 <*> defer \_ -> genJCursor
   genString = fromCharArray <$> Gen.unfoldable genChar
-  genChar = C.fromCharCode <$> Gen.chooseInt 0 65535
+  genChar = (unsafePartial fromJust <<< C.fromCharCode) <$> Gen.chooseInt 0 65535
